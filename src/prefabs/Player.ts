2e183aee5740c82523041fd4ee0fbf39e4701fa3
PhaserEditor2D <developers@phasereditor2d.com>
2022-06-29 10:08:33 -0400

    Removes log messages.

--- src/prefabs/Player.ts
+++ src/prefabs/Player.ts
@@ -56,7 +56,6 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 
 			this.play("player-Running", true);
 
-			console.log("play walk " + Date.now());
 			SoundManager.playSound(this.scene, "walk");
 
 			this._countJumps = 0;
@@ -131,7 +130,6 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 
 			this.play("player-Jump Loop", true);
 
-			console.log("stop walk" + Date.now());
 			SoundManager.stopSound(this.scene, "walk");
 
 			SoundManager.playSound(this.scene, "sfx_wing", true);
